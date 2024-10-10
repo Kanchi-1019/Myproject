dd507d38b9d746f367c26a0fa8ba062ef17a9bf6
Kanchi <Kanchi@DESKTOP-6D0GPUM>
2024-10-10 14:59:07 +0530

    project complete

---
+++ Myproject/src/main/java/testpgm/test1.java
@@ -0,0 +1,14 @@
+package testpgm;
+
+public class test1 {
+	
+	int a=10;
+	int b=30;
+public void sum()
+
+{
+	int c=a+b;// TODO Auto-generated method st
+System.out.println("Sum="+c);
+
+}
+}
---
+++ Myproject/src/main/java/testpgm/test2.java
@@ -0,0 +1,18 @@
+package testpgm;
+
+
+public class test2 extends test1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		test2 obj=new test2();
+		obj.sub();
+		obj.sum();
+	}		
+	public void sub()
+	{
+		int s=a-b;
+		System.out.println("Sub="+s);
+	}
+
+}
